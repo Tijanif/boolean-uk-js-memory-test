c68c20d0bb593fbab71b81b34a6736ba522396a7
Tijani Fulani <Fulani07@gmail.com>
2021-05-04 16:49:09 +0100

    part one done

--- index.js
+++ index.js
@@ -26,17 +26,53 @@ playMemoryTest(10) should show and ask for 10 numbers
 
 
 // Show the user 4 random numbers between 1 and 100
-function playMemoryTest (){
+function showUserRandomNumbers (){
  let arrayToShowUser = []
 while( arrayToShowUser.length < 4) {
  let randomNumber = Math.floor(Math.random() * 100) + 1
  if(arrayToShowUser.indexOf(randomNumber) === -1) arrayToShowUser.push(randomNumber)
 }
-return alert(arrayToShowUser)
+return arrayToShowUser
 }
-
-
-const intervalID = setInterval(playMemoryTest, 15000,);
-
+// console.log(showUserRandomNumbers())
 // After 15 seconds, ask them to enter each number in order
+
+ // funtion to ask user of numbers they saw
+ function whatNumbersDidYouSee (){
+  let numberUserSaw = []
+   for(let i = 0; i  < 4; i++){
+    let numberUserTyped = Number(prompt("What was the numbers you saw"))
+    numberUserSaw.push(numberUserTyped)
+   }
+   return numberUserSaw
+ }
+
+ function comparedAnswers (ramdomNumbersArray, useranswersArray) {
+  let count = 0
+   for(const randomElement of ramdomNumbersArray){
+     if (useranswersArray.includes(randomElement)){
+      count++
+     }
+   }
+   return count
+ }
+
+
+  function myMemoryTest() {
+  let randomNumbers =  showUserRandomNumbers()
+  alert(randomNumbers)
+  setTimeout(function (){
+
+    let userAnswers = whatNumbersDidYouSee()
+   let rightAnswers  = comparedAnswers(randomNumbers,userAnswers)
+   alert(`You guessed ${rightAnswers} numbers corect`)
+  }, 1000)
+  
+ }
+ // pass that function to a 15 sec timeout
+ // capture the user input
+ myMemoryTest()
+console.log("I am after the alert")
+
+
 // Let them know how many they got right!
